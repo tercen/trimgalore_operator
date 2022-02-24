64443224a423e8ae81c19e5f40aee4a74fe50caa
fcadete <filipe@cadete.eu>
2022-02-24 22:30:26 +0000

    Changed to filesystem-based approach

--- main.R
+++ main.R
@@ -2,200 +2,57 @@ library(tercen)
 library(dplyr)
 library(stringr)
 
-serialize.to.string = function(object){
-  con = rawConnection(raw(0), "r+")
-  saveRDS(object, con)
-  str64 = base64enc::base64encode(rawConnectionValue(con))
-  close(con)
-  return(str64)
-}
-deserialize.from.string = function(str64){
-  con = rawConnection(base64enc::base64decode(str64), "r+")
-  object = readRDS(con)
-  close(con)
-  return(object)
-}
-
-find.schema.by.factor.name = function(ctx, factor.name){
-  visit.relation = function(visitor, relation){
-    if (inherits(relation,"SimpleRelation")){
-      visitor(relation)
-    } else if (inherits(relation,"CompositeRelation")){
-      visit.relation(visitor, relation$mainRelation)
-      lapply(relation$joinOperators, function(jop){
-        visit.relation(visitor, jop$rightRelation)
-      })
-    } else if (inherits(relation,"WhereRelation") 
-               || inherits(relation,"RenameRelation")){
-      visit.relation(visitor, relation$relation)
-    } else if (inherits(relation,"UnionRelation")){
-      lapply(relation$relations, function(rel){
-        visit.relation(visitor, rel)
-      })
-    } 
-    invisible()
-  }
-  
-  myenv = new.env()
-  add.in.env = function(object){
-    myenv[[toString(length(myenv)+1)]] = object$id
-  }
-  
-  visit.relation(add.in.env, ctx$query$relation)
-  
-  schemas = lapply(as.list(myenv), function(id){
-    ctx$client$tableSchemaService$get(id)
-  })
-  
-  Find(function(schema){
-    !is.null(Find(function(column) column$name == factor.name, schema$columns))
-  }, schemas);
-}
-
 
 ctx <- tercenCtx()
 
+# Define input and output paths
+input_path <- "/var/lib/tercen/share/write/demultiplexed_fastqs"
 
-schema <- find.schema.by.factor.name(ctx, names(ctx$cselect())[[1]])
+if( dir.exists(input_path) == FALSE) {
 
-if (length(ctx$cselect()) == 2 ) {
-  sample_names_vector = ctx$cselect()[[2]]
-} else {
-  sample_names_vector = ctx$cselect()[[1]]
+  stop("ERROR: demultiplexed_fastqs folder does not exist in project write folder.")
+
 }
 
-table <- ctx$client$tableSchemaService$select(schema$id, Map(function(x) x$name, schema$columns), 0, schema$nRows)
+if (length(dir(input_path)) == 0) {
+  stop("ERROR: demultiplexed_fastqs folder is empty.")
+}
 
-table <- as_tibble(table)
 
-hidden_colnames <- colnames(table)[str_starts(colnames(table), "\\.")]
+output_path <- "/var/lib/tercen/share/write/trimmed_fastqs"
 
 is_paired_end <- as.character(ctx$op.value('paired_end'))
 
 if (is_paired_end == "yes") {
   
-  if (!((".forward_read_fastq_data" %in% hidden_colnames) &
-        (".reverse_read_fastq_data" %in% hidden_colnames))) {
-    
-    stop("Input is not samples containing paired-end fastq data.")
-    
-  }
+  r1_files <- list.files(input_path, "R1.fastq",
+                         full.names = TRUE)
+
+  if (length(r1_files) == 0) stop("ERROR: No R1 FastQ files found in demultiplex_fastqs folder.")
   
-  
-  output_table <- tibble()
-  
-  for (i in 1:nrow(table)) {
+  for (i in 1:length(r1_files)) {
     
-    sample_name <- sample_names_vector[[i]]
-    
-    filename_r1 <- paste0(sample_name, "1.fastq.gz")
-    filename_r2 <- paste0(sample_name, "2.fastq.gz")
-    
-    writeBin(deserialize.from.string(table[".forward_read_fastq_data"][[1]][[i]]), filename_r1)
-    writeBin(deserialize.from.string(table[".reverse_read_fastq_data"][[1]][[i]]), filename_r2)
+    r1_file <- r1_files[[i]]
+    r2_file <- str_replace(r1_file, "R1", "R2")
+
+
+    sample_name <- str_split(basename(r1_file),
+                           "_R1.fastq")[[1]][[1]]
     
     cmd <- paste("trim_galore --output_dir",
-                 paste0("output_dir_", i),
+                 output_path,
                  "--paired",
-                 filename_r1, filename_r2)
+                 r1_file, r2_file)
     
     print(cmd)
     
     system(cmd)
 
-    list.files(paste0("output_dir_", i))
-        
-    filename_val1 <- paste0("output_dir_", i, "/", sample_name, "1_val_1.fq.gz")
-    
-    print(filename_val1)
-    bytes_val1 <- readBin(file(filename_val1, 'rb'),
-                          raw(),
-                          n=file.info(filename_val1)$size)
-    
-    filename_val2 <- paste0("output_dir_", i, "/", sample_name, "2_val_2.fq.gz")
-    print(filename_val2)
-    bytes_val2 <- readBin(file(filename_val2, 'rb'),
-                          raw(),
-                          n=file.info(filename_val2)$size)
-    
-    string_val1 <- serialize.to.string(bytes_val1)
-    string_val2 <- serialize.to.string(bytes_val2)
-    
-    output_table <- bind_rows(output_table,
-                              tibble(sample = sample_name,
-                                     .forward_read_fastq_data = string_val1,
-                                     .reverse_read_fastq_data = string_val2))
-    
   }
   
-  
-} else if (is_paired_end == "no") {
-  
-  if (!(".single_end_fastq_data" %in% hidden_colnames)) {
-    
-    stop("Input is not samples containing single-end fastq data.")
-    
-  }
-  
-  
-  output_table <- tibble()
-  
-  for (i in 1:nrow(table)) {
-    
-    sample_name <- sample_names_vector[[i]]
-    
-    filename <- sample_name
-    
-    writeBin(deserialize.from.string(table[".single_end_fastq_data"][[1]][[i]]), filename)
-    
-    cmd <- paste("trim_galore --output_dir",
-                 paste0("output_dir_", i),
-                 filename)
-    
-    system(cmd)
-    
-    filename_trimmed <- list.files(paste0("output_dir_", i),
-                                   pattern = "*fq.gz",
-                                   full.names = TRUE)[[1]]
-    
-    bytes_trimmed <- readBin(file(filename_trimmed, 'rb'),
-                             raw(),
-                             n=file.info(filename_trimmed)$size)
-    
-    string_trimmed <- serialize.to.string(bytes_trimmed)
-    
-    output_table <- bind_rows(output_table,
-                              tibble(sample = sample_name,
-                                     .single_end_fastq_data = string_trimmed))
-    
-  }
   
 }
 
-
-save_output <- as.character(ctx$op.value('save_output_to_folder'))
-
-if (save_output == "yes") {
-  
-  output_folder_prefix <- as.character(ctx$op.value('output_folder_prefix'))
-  
-  # create trim galore zipped output
-  system("zip -r trim_galore_output.zip output_dir_*")
-  
-  # save zipped file to project folder
-  filename <- "trim_galore_output.zip"
-  bytes = readBin(file(filename, 'rb'),
-                  raw(),
-                  n = file.info(filename)$size)
-  
-  fileDoc = FileDocument$new()
-  fileDoc$name = paste0(output_folder_prefix, "_", filename)
-  fileDoc$projectId = ctx$cschema$projectId
-  fileDoc$size = length(bytes)
-  
-  fileDoc = ctx$client$fileService$upload(fileDoc, bytes)
-  
-}
                                                     
 output_table %>%
   mutate(.ci = 1,
